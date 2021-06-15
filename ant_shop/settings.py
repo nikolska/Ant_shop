1da08d2f8e9aa8470f2b083680751d9417cd9597
nikolska <nikolska1995@gmail.com>
2021-06-15 20:15:47 +0200

    change email

--- ant_shop/settings.py
+++ ant_shop/settings.py
@@ -114,10 +114,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Email send
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'nikolska.work@onet.pl'
-EMAIL_HOST_PASSWORD = 'Dontrepeatyourself48'
-EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.mail.ru'
+EMAIL_HOST_USER = 'nikolska.work@mail.ru'
+EMAIL_HOST_PASSWORD = 'Dontrepeatyourself'
+EMAIL_PORT = 2525
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
